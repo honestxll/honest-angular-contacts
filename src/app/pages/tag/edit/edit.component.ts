c2b6f9b78e8e9b77692592cb3c96c7e10c906934
honest <1131328108@qq.com>
2019-10-14 13:51:08 +0800

    编辑标签

--- src/app/pages/tag/edit/edit.component.ts
+++ src/app/pages/tag/edit/edit.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder } from '@angular/forms';
+import { TagService } from 'src/app/services/tag.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Toast } from 'src/app/utils/toast';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.less']
+  styleUrls: ['./edit.component.less'],
 })
 export class EditComponent implements OnInit {
-
-  constructor() { }
+  tagId = this.route.snapshot.params.id;
+  tagForm = this.formBuilder.group({
+    title: ['', Validators.required],
+  });
+  constructor(
+    private formBuilder: FormBuilder,
+    private tagService: TagService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngOnInit() {
+    this.tagService.show(this.tagId).subscribe((tag: Tag) => {
+      const { id, ...newTag } = tag;
+      this.tagForm.setValue(newTag);
+    });
   }
 
+  get title() {
+    return this.tagForm.get('title');
+  }
+
+  onSubmit() {
+    this.tagService.update(this.tagId, this.tagForm.value).subscribe(() => {
+      Toast.fire({
+        type: 'success',
+        title: '保存成功',
+      });
+    });
+  }
 }
